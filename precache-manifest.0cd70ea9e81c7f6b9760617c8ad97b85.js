19d2f32088895b9ee19c19cc412a57b716f4f298
Kyulee <yunguyen94@gmail.com>
2020-06-07 22:10:01 +0200

    Updates

---
+++ precache-manifest.0cd70ea9e81c7f6b9760617c8ad97b85.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "1caf0789ee8fdb1f0b3c",
+    "url": "/me/static/js/runtime~main.1caf0789.js"
+  },
+  {
+    "revision": "07c9b806db5138ad7f6a",
+    "url": "/me/static/js/main.07c9b806.chunk.js"
+  },
+  {
+    "revision": "5ec21d738fba382f2658",
+    "url": "/me/static/js/2.5ec21d73.chunk.js"
+  },
+  {
+    "revision": "07c9b806db5138ad7f6a",
+    "url": "/me/static/css/main.8e859488.chunk.css"
+  },
+  {
+    "revision": "d482c8b9bc398a0a22fc262dbb5b9686",
+    "url": "/me/index.html"
+  }
+];
